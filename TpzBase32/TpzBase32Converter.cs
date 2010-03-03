2234243ca718893aa395e0e3c59ccddf0a81a321
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-02 16:38:27 -0800

    Added a bit of doc to ConvertInt32ToEnumerableOfBoolean, as much as it won't be used directly.

--- TpzBase32/TpzBase32Converter.cs
+++ TpzBase32/TpzBase32Converter.cs
@@ -5,6 +5,15 @@ namespace twopointzero.TpzBase32
 {
     public static class TpzBase32Converter
     {
+        /// <summary>
+        /// ConvertInt32ToEnumerableOfBoolean converts the raw bit representation
+        /// of the provided Int32 input argument into an enumerable of Boolean
+        /// values with true members of the result representing the high bits
+        /// in the input value, from LSb to MSb.
+        /// </summary>
+        /// <param name="input">The Int32 value whose raw bit states will be returned in enumerable form.</param>
+        /// <returns>An enumerable of Boolean values representing the bit states of the input argument,
+        /// ordered from least-significant bit to most-significant bit.</returns>
         public static IEnumerable<bool> ConvertInt32ToEnumerableOfBoolean(int input)
         {
             for (int i = 0; i < 32; i++)
