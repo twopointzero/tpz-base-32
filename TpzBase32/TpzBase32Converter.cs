2082f2a312a4f5b90a954f64658472e3a1cd27fa
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-02 16:49:35 -0800

    Simplified conversion method name so further methods can also have simpler and more consistent names.

--- TpzBase32/TpzBase32Converter.cs
+++ TpzBase32/TpzBase32Converter.cs
@@ -6,15 +6,17 @@ namespace twopointzero.TpzBase32
     public static class TpzBase32Converter
     {
         /// <summary>
-        /// ConvertInt32ToEnumerableOfBoolean converts the raw bit representation
+        /// ConvertToBitEnumerable converts the raw bit representation
         /// of the provided Int32 input argument into an enumerable of Boolean
         /// values with true members of the result representing the high bits
-        /// in the input value, from LSb to MSb.
+        /// in the input value, from LSb to MSb. This method can be considered
+        /// a rough analog to the BitArray that already exists within the framework,
+        /// though represented in terms of generics.
         /// </summary>
         /// <param name="input">The Int32 value whose raw bit states will be returned in enumerable form.</param>
         /// <returns>An enumerable of Boolean values representing the bit states of the input argument,
         /// ordered from least-significant bit to most-significant bit.</returns>
-        public static IEnumerable<bool> ConvertInt32ToEnumerableOfBoolean(int input)
+        public static IEnumerable<bool> ConvertToBitEnumerable(int input)
         {
             for (int i = 0; i < 32; i++)
             {
