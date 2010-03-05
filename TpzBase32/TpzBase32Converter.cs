f14d61cd89cba64556d7f9fe8fdab6febd1e6129
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-05 11:49:25 -0800

    Implemented ConvertToQuintetEnumerable, with tests. (Primarily for internal usage.)

--- TpzBase32/TpzBase32Converter.cs
+++ TpzBase32/TpzBase32Converter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace twopointzero.TpzBase32
 {
@@ -20,6 +22,68 @@ namespace twopointzero.TpzBase32
             for (int i = 0; i < 32; i++)
             {
                 yield return (((uint)input >> i) & 1) == 1;
+            }
+        }
+
+        /// <summary>
+        /// ConvertToQuintetEnumerable converts an enumerable of boolean values
+        /// representing bits into an enumerable of bytes each of which represents
+        /// (in its low bits) five bits from the input enumerable.
+        /// </summary>
+        /// <param name="input">A non-null enumerable of booleans representing bits.</param>
+        /// <returns>A non-null enumerable of zero or more bytes, with each byte
+        /// containing five bits worth of entries from the input enumerable.</returns>
+        /// <remarks>The input enumerable can include any number of bits, not
+        /// just in multiples of five, but always produces results which to the
+        /// recipient may be indistinguishable from an equivalent input
+        /// enumerable with additional false values. When round-tripping using
+        /// these methods, additional steps will need to be taken externally
+        /// in order to account for input enumerables containing a number of
+        /// values that is not an even multiple of five.</remarks>
+        public static IEnumerable<byte> ConvertToQuintetEnumerable(IEnumerable<bool> input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            // We need to delgate to the method below so that the argument
+            // check above runs immediately. If our yield calls were in this
+            // method, the compiler's iterator support would defer the argument
+            // until the first MoveNext call against the returned enumerable.
+            return ConvertToQuintetEnumerableImpl(input);
+        }
+
+        /// <summary>
+        /// ConvertToQuintetEnumerableImpl provides the iterator implementation
+        /// that services ConvertToQuintetEnumerable, allowing ConvertToQuintetEnumerable
+        /// to immediately perform input validation when called, instead of
+        /// accidentally deferring it until the first enumerator MoveNext call;
+        /// </summary>
+        private static IEnumerable<byte> ConvertToQuintetEnumerableImpl(IEnumerable<bool> input)
+        {
+            if (!input.Any())
+            {
+                yield break;
+            }
+
+            byte accumulator = 0;
+            byte offset = 0;
+            var enumerator = input.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                byte current = enumerator.Current ? (byte)1 : (byte)0;
+                accumulator |= (byte)(current << offset++);
+                if (offset == 5)
+                {
+                    yield return accumulator;
+                    accumulator = 0;
+                    offset = 0;
+                }
+            }
+            if (offset != 0)
+            {
+                yield return accumulator;
             }
         }
     }
