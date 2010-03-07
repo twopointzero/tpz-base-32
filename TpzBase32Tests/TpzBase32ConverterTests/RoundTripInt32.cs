d8a4dea48931bddb367a70a5c431755d7fa49800
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-06 16:20:41 -0800

    Added long-running (one million iterations) seeded pseudo-random round-trip tests from int->string->int both without and with padding.

---
+++ TpzBase32Tests/TpzBase32ConverterTests/RoundTripInt32.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using twopointzero.TpzBase32;
+
+namespace twopointzero.TpzBase32Tests.TpzBase32ConverterTests
+{
+    [TestFixture]
+    public class RoundTripInt32
+    {
+        [Test, Explicit("Long-running")]
+        public void RoundTrip()
+        {
+            var rnd = new Random(42);
+            for (long i = 0; i <= 1000000; i++)
+            {
+                var input = rnd.Next(int.MinValue, int.MaxValue);
+                var encoded = TpzBase32Converter.Encode(input);
+                var decoded = TpzBase32Converter.DecodeToInt32(encoded);
+                if (input != decoded)
+                {
+                    Assert.AreEqual(input, decoded);
+                }
+            }
+        }
+
+        [Test, Explicit("Long-running")]
+        public void RoundTripWithPadding()
+        {
+            var rnd = new Random(42);
+            for (long i = 0; i <= 1000000; i++)
+            {
+                var input = rnd.Next(int.MinValue, int.MaxValue);
+                var encoded = TpzBase32Converter.EncodeWithPadding(input);
+                var decoded = TpzBase32Converter.DecodeToInt32(encoded);
+                if (input != decoded)
+                {
+                    Assert.AreEqual(input, decoded);
+                }
+            }
+        }
+    }
+}
