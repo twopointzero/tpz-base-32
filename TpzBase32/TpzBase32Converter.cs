b6a4e757527ca4e70c5c0d526cb23f8fd0b2bf20
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-08 15:59:31 -0800

    Exposed Normalize(string) and Normalize(char) from TpzBase32Converter as convenience methods (delegating to the internal extension methods) useful when validating end user input to confirm what they are attempting to enter and/or to coerce values as they enter them.

--- TpzBase32/TpzBase32Converter.cs
+++ TpzBase32/TpzBase32Converter.cs
@@ -82,5 +82,48 @@ namespace twopointzero.TpzBase32
 
             return input.DecodeToQuintets().UnpackQuintets().PackToInt32s().First();
         }
+
+        /// <summary>
+        /// Normalize processes the provided input string, correcting errors
+        /// that commonly occur during the human transcription of numbers
+        /// and characters. 0 (the number zero) becomes o (the lowercase
+        /// letter O), l (the lowercase letter L) becomes 1 (the number one),
+        /// 2 (the number two) becomes z (the lowercase letter Z), and v (the
+        /// lowercase letter V) becomes u (the lowercase letter U), as these
+        /// are all common transcription errors. All uppercase letters become
+        /// lowercase, as the encoding is lowercase in nature.
+        /// </summary>
+        /// <param name="input">A non-null string containing zero or more
+        /// characters that are expected to represent an encoded value.</param>
+        /// <returns>A new string </returns>
+        /// <remarks>This normalization process assumes that the entire input
+        /// string is to be processed as an encoded value, and is not intended
+        /// It will accept a string containing arbitrary text just a portion
+        /// of which is a value in the encoded representation, and can be used
+        /// to assist in locating encoded values within other larger pieces of
+        /// text, but as a result it may modify other portions of the input
+        /// string in unexpected ways.</remarks>
+        public static string Normalize(this string input)
+        {
+            return input.NormalizeToTpzBase32Alphabet();
+        }
+
+        /// <summary>
+        /// Normalize processes the provided input char, correcting errors
+        /// that commonly occur during the human transcription of numbers
+        /// and characters. 0 (the number zero) becomes o (the lowercase
+        /// letter O), l (the lowercase letter L) becomes 1 (the number one),
+        /// 2 (the number two) becomes z (the lowercase letter Z), and v (the
+        /// lowercase letter V) becomes u (the lowercase letter U), as these
+        /// are all common transcription errors. All uppercase letters become
+        /// lowercase, as the encoding is lowercase in nature.
+        /// </summary>
+        /// <param name="input">A char containing a 
+        /// character that is expected to represent an encoded value.</param>
+        /// <returns>The input character, normalized if it was required.</returns>
+        public static char Normalize(this char input)
+        {
+            return input.NormalizeToTpzBase32Alphabet();
+        }
     }
 }
