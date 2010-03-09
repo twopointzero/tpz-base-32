ef9be7ede8fcd0a49d3daa8a05730d6a91aacc5f
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-08 18:37:46 -0800

    Added an initial burn-in test program. This console program currently compares the behaviour of the Int32 and Reference converters, round-tripped through both padded and unpadded encodings, across the entire Int32 value range. Currently a single-threaded implementation running for approx. seven hours, but can easily be parallelized if the burn-in needs to be run again.

---
+++ TpzBase32BurnIn/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using twopointzero.TpzBase32;
+
+namespace twopointzero.TpzBase32BurnIn
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = int.MinValue; i <= int.MaxValue; i++)
+            {
+                if (i % 1000000 == 0)
+                {
+                    TimeSpan elapsed = stopwatch.Elapsed;
+                    var ticks = elapsed.Ticks;
+                    const long range = (long)int.MaxValue - int.MinValue;
+                    var step = i - int.MinValue;
+                    var ticksPerStep = ticks / step;
+                    var stepsRemaining = range - step;
+                    var ticksRemaining = ticksPerStep * stepsRemaining;
+                    Console.WriteLine(string.Format("{0}: {1} (ETA {2}.)", elapsed, i,
+                                                    TimeSpan.FromTicks(ticksRemaining)));
+                }
+
+                int input = i;
+
+                var int32EncodedWithPadding = Int32Converter.EncodeWithPadding(input);
+                var referenceEncodedWithPadding = ReferenceConverter.EncodeWithPadding(input);
+                if (int32EncodedWithPadding != referenceEncodedWithPadding)
+                {
+                    Console.WriteLine("Int32 encoded with padding differs from Reference for original input of " + input);
+                    Console.WriteLine("Int32 encoded:     " + int32EncodedWithPadding);
+                    Console.WriteLine("Reference encoded: " + referenceEncodedWithPadding);
+                    break;
+                }
+
+                var int32Encoded = Int32Converter.Encode(input);
+                var referenceEncoded = ReferenceConverter.Encode(input);
+                if (int32Encoded != referenceEncoded)
+                {
+                    Console.WriteLine("Int32 encoded differs from Reference for original input of " + input);
+                    Console.WriteLine("Int32 encoded:     " + int32Encoded);
+                    Console.WriteLine("Reference encoded: " + referenceEncoded);
+                    break;
+                }
+
+                var int32DecodedFromPadded = Int32Converter.DecodeToInt32(int32EncodedWithPadding);
+                var referenceDecodedFromPadded = ReferenceConverter.DecodeToInt32(int32EncodedWithPadding);
+                if (int32DecodedFromPadded != referenceDecodedFromPadded)
+                {
+                    Console.WriteLine("Int32 decoded from padded differs from Reference for original input of " + input);
+                    Console.WriteLine("Encoded:           " + int32EncodedWithPadding);
+                    Console.WriteLine("Int32 decoded:     " + int32DecodedFromPadded);
+                    Console.WriteLine("Reference decoded: " + referenceDecodedFromPadded);
+                    break;
+                }
+
+                var int32Decoded = Int32Converter.DecodeToInt32(int32Encoded);
+                var referenceDecoded = ReferenceConverter.DecodeToInt32(int32Encoded);
+                if (int32Decoded != referenceDecoded)
+                {
+                    Console.WriteLine("Int32 decoded differs from Reference for original input of " + input);
+                    Console.WriteLine("Encoded:           " + int32Encoded);
+                    Console.WriteLine("Int32 decoded:     " + int32Decoded);
+                    Console.WriteLine("Reference decoded: " + referenceDecoded);
+                    break;
+                }
+
+                if (input != int32DecodedFromPadded)
+                {
+                    Console.WriteLine("Decoded from padded differs from original input of " + input);
+                    Console.WriteLine("Encoded: " + int32EncodedWithPadding);
+                    Console.WriteLine("Decoded: " + int32DecodedFromPadded);
+                    break;
+                }
+
+                if (input != int32Decoded)
+                {
+                    Console.WriteLine("Decoded differs from original input of " + input);
+                    Console.WriteLine("Encoded: " + int32Encoded);
+                    Console.WriteLine("Decoded: " + int32Decoded);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TpzBase32BurnIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("twopointzero.TpzBase32BurnIn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("twopointzero.TpzBase32BurnIn")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("7590d0fb-1406-4b83-b47a-f73c88d2edb0")]
